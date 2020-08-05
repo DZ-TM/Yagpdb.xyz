5c4c10960e3a4b37b5e931324729949d1cf0e30e
DZ <69156423+DZ-TM@users.noreply.github.com>
2020-08-05 19:08:15 +0100

    Create 10m Interval.lua

---
+++ Commands/Bump System/10m Interval.lua
@@ -0,0 +1,17 @@
+{{/*
+	Made by DZ#6669 (438789314101379072)
+
+	Trigger Type: Interval
+	Time: 10m
+*/}}
+
+{{/* configuration area */}}
+{{$channelid := 740556133038555246}}{{/* ID of the channel you want it to edit the name of, recommended to use a voice channel */}}
+
+{{/* do not edit below (unless you know what you're doing c: ) */}}
+{{$db:=dbGet 0 "bump"}}
+{{if $db}}
+	{{editChannelName $channelid (print "Next Bump in " (((dbGet 0 "bump").ExpiresAt.Sub currentTime).Round .TimeSecond))}}
+{{else}}
+	{{editChannelName $channelid "Bump Now!"}}
+{{end}}
