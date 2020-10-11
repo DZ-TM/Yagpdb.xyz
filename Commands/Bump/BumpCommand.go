25dee2ccf92e70e6581fe148c3c21814d448c9d9
DZ <69156423+DZ-TM@users.noreply.github.com>
2020-10-11 19:55:38 +0100

    Update BumpCommand.go

--- Commands/Bump/BumpCommand.go
+++ Commands/Bump/BumpCommand.go
@@ -6,17 +6,21 @@
 */}}
 
 {{/* configuration area */}}
-{{$voicechannelid := 740556133038555246}}
-{{$message := "Thanks for bumping us!"}}
+{{$voiceChannelID := 740556133038555246}}
+{{$thanksMessage := "Thanks for bumping us!"}}{{/* thanks message if the user successfully bumps the server */}}
+{{$bumpChannel := 764595403219664898}}{{/* channel to send the message telling users it is possible to bump again */}}
+{{$bumpPing := "<@&724944927330533407>"}}{{/* role to ping when it is possible to bump once again */}}
+{{$bumpMessage := "Please bump us, your support is appreciated!"}}{{/* message to be sent when it is possible for the server to be bumped once again */}}
 
 {{/* do not edit below (unless you know what you're doing c: ) */}}
 {{if .ExecData}}
-	{{editChannelName $voicechannelid "Bump Now!"}}
+	{{editChannelName $voiceChannelID "Bump Now!"}}
+	{{sendMessage $bumpChannel (complexMessage "content" $bumpPing "embed" (cembed "title" "Bump" "description" $bumpMessage))}}
 {{else}}
 	{{if not (dbGet 0 "bump")}}
 		{{dbSetExpire 0 "bump" 1 7200}}
-		{{editChannelName $voicechannelid (print "Next Bump in " (((dbGet 0 "bump").ExpiresAt.Sub currentTime).Round .TimeSecond))}}
+		{{editChannelName $voiceChannelID (print "Next Bump in " (((dbGet 0 "bump).ExpiresAt.Sub currentTime).Round .TimeSecond))}}
 		{{execCC .CCID nil 7200 "data"}}
-		{{sendMessage nil (cembed "title" "Bump!" "description" $message)}}
+		{{sendMessage nil (cembed "title" "Bump!" "description" $thanksMessage)}}
 	{{end}}
 {{end}}
