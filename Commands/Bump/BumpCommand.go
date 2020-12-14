6a09da32887c172a000f5fbe9471c757fad5a522
Chiranthan Bharadwaj R <chiranthan568@gmail.com>
2020-12-14 22:25:17 +0530

    Update BumpCommand.go

--- Commands/Bump/BumpCommand.go
+++ Commands/Bump/BumpCommand.go
@@ -1,26 +1,43 @@
-{{/*
-	Made by DZ#6669 (438789314101379072)
-
-	Trigger Type: RegEx
-	Trigger: \A!d\sbump(?:\s+|\z)
+{{*/
+    
+  Usage `!d bump/lb/rank`
+  Recommended trigger: `!d`
+	Trigger type: Regex
+  Credits: WickedWizard#3588  
+  Also, please change the ROLEID on line 31.
+  Please read through the code properly and try to learn, rather than copying. Same goes for the other cc's given here too
+  
 */}}
 
-{{/* configuration area */}}
-{{$voiceChannelID := 740556133038555246}}{{/* channel at which the bot edits its name */}}
-{{$thanksMessage := "Thanks for bumping us!"}}{{/* thanks message if the user successfully bumps the server */}}
-{{$bumpChannel := 764595403219664898}}{{/* channel to send the message telling users it is possible to bump again */}}
-{{$bumpPing := "<@&724944927330533407>"}}{{/* role to ping when it is possible to bump once again */}}
-{{$bumpMessage := "Please bump us, your support is appreciated!"}}{{/* message to be sent when it is possible for the server to be bumped once again */}}
+{{/*Configuration Variables Start*/}}
+{{$url := "https://disboard.org/server/721592235359207545"}} {{/*Your Servers Disboard URL*/}}
+{{$reminderchannel := 786145239466639360}}{{/*Channel ID where Remainders are to be sent every hour*/}}
+{{$bumprole := 787355830399270963}}
+{{/*Configuration Values End*/}}
 
-{{/* do not edit below (unless you know what you're doing c: ) */}}
-{{if .ExecData}}
-	{{editChannelName $voiceChannelID "Bump Now!"}}
-	{{sendMessageNoEscape $bumpChannel (complexMessage "content" $bumpPing "embed" (cembed "title" "Bump" "description" $bumpMessage))}}
-{{else}}
-	{{if not (dbGet 0 "bump")}}
-		{{dbSetExpire 0 "bump" 1 7200}}
-		{{editChannelName $voiceChannelID "Next Bump in 2h0m0s"}}
-		{{execCC .CCID nil 7200 "data"}}
-		{{sendMessage nil (cembed "title" "Bump!" "description" $thanksMessage)}}
-	{{end}}
-{{end}}
+{{$args := parseArgs 1 "" (carg "string" "Function Type")}}
+{{$BumpXP := dbGet .User.ID "BumpXP"}}
+{{if eq ($args.Get 0) "bump"}}
+    {{if $cooldown := dbGet 0 "Cooldown"}}
+        HELP
+        {{deleteResponse 0}}
+    {{else}}
+        {{sleep 1}}
+        {{execAdmin "clean" 1}}
+        {{dbSetExpire 0 "Cooldown" "Cooldown" 60}}
+        {{$success := sendMessageRetID nil (cembed "title" "DISBOARD: The Public Server List" "url" $url "description" (print .User.Mention ", \nBump Done👍\n[Check it Here](" $url ") \nYou are bump level " $BumpXP.Value) "color" 4229631  "image" (sdict "url" "https://i.imgur.com/PvJZE7j.png") "footer" (sdict "text" "React with 🔔 to configure your Bump Pings"))}}
+        {{addMessageReactions nil $success "🔔"}}
+        {{$t1 := dbIncr .User.ID "BumpXP" 1}}
+        {{editMessage $reminderchannel (toInt (dbGet 1 "Bump").Value) (print "~~<@&" $bumprole "> 🔔 You can Bump again Now!~~"}} {{/*Update the RoleID here*/}}
+    {{end}}
+{{else if eq ($args.Get 0) "lb"}}
+        {{$rank := 0}}
+        {{$list:=""}}
+{{range dbTopEntries "BumpXP" 10 0}}
+        {{ $rank = add $rank 1 }}
+        {{- $list =print $list "**" $rank ".** " .User.Username "** - **" .Value "\n" -}}
+    {{end}}
+        {{$s := sendMessage nil (cembed "title" (joinStr "" "🏆Bumping LeaderBoard for" .Guild.Name "") "description" $list)}}
+    {{else if eq ($args.Get 0) "rank"}}
+Your Bump Level is {{$BumpXP.Value}}.
+    {{end}} 
