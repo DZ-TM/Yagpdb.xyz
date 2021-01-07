7d17b1a6d0aa0b2f98ea1ba16f3048b7976d77a1
DZ <69156423+DZ-TM@users.noreply.github.com>
2021-01-07 21:50:50 +0000

    Create cmd.go

---
+++ Commands/reverse/cmd.go
@@ -0,0 +1,37 @@
+{{/*
+	Made by DZ#6669 (438789314101379072)
+	Trigger Type: Command
+	Trigger: reverse
+*/}}
+{{/* configuration area */}}
+{{$fail:=sdict "color" 14565697}}
+{{$success:=sdict "color" 586350}}
+
+{{/* do not edit below */}}
+{{$result:=""}}
+{{define "send"}}
+    {{range $k,$v:=.change}}
+        {{- $.embed.Set $k $v -}}
+    {{end}}
+    {{sendMessage nil (cembed .embed)}}
+{{end}}
+{{if .StrippedMsg}}
+    {{range $i,$_:=($split:=split .StrippedMsg "")}}
+        {{- $result =print $result (sub (len $split) 1 $i|index $split) -}}
+    {{end}}
+    {{template "send" (sdict
+        "embed" $success
+        "change" (sdict
+            "title" "Reversed Text:"
+            "description" $result
+        )
+    )}}
+{{else}}
+    {{template "send" (sdict
+        "embed" $fail
+        "change" (sdict
+            "title" "No Text Entered"
+            "description" "Please enter some arguments to reverse.\n```\n-reverse [Text]\n```"
+        )
+    )}}
+{{end}}
