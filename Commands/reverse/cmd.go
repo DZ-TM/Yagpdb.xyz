26a929458c9f607de25d34abc8fc0b4fa60aa652
DZ <69156423+DZ-TM@users.noreply.github.com>
2021-01-07 22:02:56 +0000

    Update cmd.go

--- Commands/reverse/cmd.go
+++ Commands/reverse/cmd.go
@@ -1,7 +1,7 @@
 {{/*
 	Made by DZ#6669 (438789314101379072)
 	Trigger Type: Command
-	Trigger: reverse
+	Trigger: username
 */}}
 {{/* configuration area */}}
 {{$fail:=sdict "color" 14565697}}
@@ -10,28 +10,28 @@
 {{/* do not edit below */}}
 {{$result:=""}}
 {{define "send"}}
-    {{range $k,$v:=.change}}
-        {{- $.embed.Set $k $v -}}
-    {{end}}
-    {{sendMessage nil (cembed .embed)}}
+	{{range $k,$v:=.change}}
+		{{- $.embed.Set $k $v -}}
+	{{end}}
+	{{sendMessage nil (cembed .embed)}}
 {{end}}
 {{if .StrippedMsg}}
-    {{range $i,$_:=($split:=split .StrippedMsg "")}}
-        {{- $result =print $result (sub (len $split) 1 $i|index $split) -}}
-    {{end}}
-    {{template "send" (sdict
-        "embed" $success
-        "change" (sdict
-            "title" "Reversed Text:"
-            "description" $result
-        )
-    )}}
+	{{range $i,$_:=($split:=split .StrippedMsg "")}}
+		{{- $result =print $result (sub (len $split) 1 $i|index $split) -}}
+	{{end}}
+	{{template "send" (sdict
+		"embed" $success
+		"change" (sdict
+			"title" "Reversed Text:"
+			"description" $result
+		)
+	)}}
 {{else}}
-    {{template "send" (sdict
-        "embed" $fail
-        "change" (sdict
-            "title" "No Text Entered"
-            "description" "Please enter some arguments to reverse.\n```\n-reverse [Text]\n```"
-        )
-    )}}
+	{{template "send" (sdict
+		"embed" $fail
+		"change" (sdict
+			"title" "No Text Entered"
+			"description" "Please enter some arguments to reverse.\n```\n-reverse [Text]\n```"
+		)
+	)}}
 {{end}}
