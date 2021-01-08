ca61a436c74543aaea2e9f363c464c1def20d7bd
DZ <69156423+DZ-TM@users.noreply.github.com>
2021-01-08 20:32:21 +0000

    Update command.go
    changed the green colour to a nicer one

--- Commands/pagination/command.go
+++ Commands/pagination/command.go
@@ -2,7 +2,7 @@
 	Made by DZ#6669 (438789314101379072)
 	Trigger Type: Command
 	Trigger of your choice.
- 
+
 	NOTE: Read README.md for instructions on configuration.
 */}}
 
@@ -11,8 +11,8 @@
 	"title" "Title A"
 	"description" "Description A"
 	"image" (sdict "url" "https://m.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/05/16/Pictures/_1571873a-58de-11e8-b431-73159b4b09e2.jpg")
-	"color" 123456
- }}{{/* first element of the slice in the reactionListener */}}
+	"color" 586350
+}}
 
 {{/* do not edit below */}}
 {{$embed.Set "description" (print (or $embed.description "") "[](0)")}}
