d2dfd24dfda11ea1e3cae78d40ff8552d8098dd4
DZ <69156423+DZ-TM@users.noreply.github.com>
2021-01-08 18:27:58 +0000

    Update command.go

--- Commands/pagination/command.go
+++ Commands/pagination/command.go
@@ -12,7 +12,7 @@
 	"description" "Description A"
 	"image" (sdict "url" "https://m.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/05/16/Pictures/_1571873a-58de-11e8-b431-73159b4b09e2.jpg")
 	"color" 123456
-}}
+ }}{{/* first element of the slice in the reactionListener */}}
 
 {{/* do not edit below */}}
 {{$embed.Set "description" (print (or $embed.description "") "[](0)")}}
