4d4cc127757258c7f249ed58d2624c7d9249f7c9
Mango White Enclavera <75037449+mangowhite@users.noreply.github.com>
2021-08-05 20:29:19 -0300

    Updated HowGay w/new Features
    • Added Top 10 Gays in the server.
    • Indented the code.
    • Better visuals.
    • Added 100% Gay easter-egg (line 40).
    
    Made this 'cause I was bored and I wanted to play with my friend DZ!

--- Commands/HowGay/HowGay.lua
+++ Commands/HowGay/HowGay.lua
@@ -1,10 +1,48 @@
-{{$howGay:=randInt 0 101}}{{$po:=0}}{{$co:=randInt 16777216}}{{$user:=.User}}{{$member:=.Member}}
-{{if .Message.Mentions}}
-	{{$user =index .Message.Mentions 0}}{{$member =getMember $user}}
+{{/*
+
+MADE BY: DZ (438789314101379072)
+EDITTED BY: mangowhite (209208777365389312)
+SUGGESTED TRIGGER: howgay
+
+DESCRIPTION: This CC gives the user an amount of Gayness that will attach to them like a parasite that will never go away. But that's not all... it will also display all the most gays in the server in a Top. 
+
+/*}}
+
+{{/*VARIABLES*/}}
+
+{{$howGay:=randInt 0 101}}
+{{$po:=0}}
+{{$color:=randInt 16777216}}
+{{$user:= ""}}{{with .Message.Mentions}}{{$user = userArg (index . 0)}}{{end}}
+{{$embed:= sdict "color" $color "title" "How Gay"}}
+
+
+{{/*CODE*/}}
+{{if $user}}
+	{{range .Guild.Roles}}
+		{{if and (in (getMember $user.ID).Roles .ID) .Color (lt $po .Position)}}
+			{{$po = .Position}}
+			{{$color = .Color}}
+		{{end}}
+	{{end}}
+	{{if not ($db:=dbGet $user.ID "howGay")}}
+		{{dbSet $user.ID "howGay" $howGay}}
+	{{else if $db}}
+		{{$list:= ""}}
+		{{$num:= 0}}
+		{{range (dbTopEntries "howGay" 10 0)}}
+			{{$num = add $num 1}}
+			{{$list = print $list $num " • " .User.Username " - " .Value "%\n"}}
+		{{end}}
+		{{$embed.Set "fields" (cslice (sdict "name" "The Most Gays" "value" $list))}}
+		{{$embed.Set "description" (print (or $user.Username (getMember $user.ID).Nick) " is " $db.Value "% gay!  🏳️‍🌈")}}
+	{{else if (ge $db 100)}}
+		{{$embed.Set "image" (sdict "url" "https://cdn.discordapp.com/attachments/832547182858862602/872972692469461023/unknown.png")}}
+	{{end}}
+	{{sendMessage nil (complexMessage 
+			"content" $user.Mention 
+			"embed" (cembed $embed)
+		)}}
+{{else}}
+	❌ You need to input a valid user mention!
 {{end}}
-{{range .Guild.Roles}}
-	{{- if and (in $member.Roles .ID) .Color (lt $po .Position)}}
-		{{- $po =.Position}}{{$co =.Color}}
-	{{- end -}}
-{{end}}
-{{sendMessage nil (complexMessage "content" $user.Mention "embed" (cembed "color" $co "title" "gay r8 machine" "description" (print $user.Username " is " $howGay "% gay 🏳️‍🌈")))}}
